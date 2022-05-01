cb8615ca2973670dfb7fdfa342f2c4ba897f61a4
JaneethReddy Alle <76918142+JaneethReddy@users.noreply.github.com>
2022-05-01 21:55:07 +0530

    Update f_n_fun.py

--- f_n_fun.py
+++ f_n_fun.py
@@ -6,7 +6,7 @@ def f(n):
     else:
         return  9*f(n-1) - 11*f(n-2)
 
-li = [23,1,4,5,245,298,4009,30240,228061,12093829]
+
 def seq_of_num(maxi):
     li = []
     for i in range(maxi//2):
@@ -23,5 +23,6 @@ def is_part_of_series(lst):
             in_list.append(i)
     return in_list
 
+li = [23,1,4,5,245,298,4009,30240,228061,12093829]
 print(is_part_of_series(li))
 
